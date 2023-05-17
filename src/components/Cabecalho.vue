03fe05a3998844e2e722fe9354d29940d822db01
Jailson Joventino <jjs2@hotmail.com>
2023-05-17 10:40:31 -0300

    Projeto Calc Reativa

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,13 @@
+<template>
+
+  <header>
+    <h1>Calculadora Rativa</h1>
+  </header>
+
+</template>
+
+<style scoped>
+  header {
+    padding: 30px 0px 10px;
+  }
+</style>
---
+++ src/components/Operacao.vue
@@ -0,0 +1,28 @@
+<script setup>
+  const props = defineProps(['operacao', 'num1', 'num2'])
+</script>
+
+<template>
+  <div class="operation">
+    <h2>Seleciona e operação para o resultado</h2>
+    <select @change="props.operacao">
+      <option value="+">+</option>
+      <option value="-">-</option>
+      <option value="*">*</option>
+      <option value="/">/</option>
+    </select>
+  </div>
+  <div>
+    <input @keyup="props.num1" required type="number" placeholder="Digite o N."/>
+    <input @keyup="props.num2" required type="number" placeholder="Digite o N."/>
+  </div>
+
+</template>
+
+<style>
+  input {
+    margin: 30px;
+    padding: 4px;
+    border-radius: 4px;
+  }
+</style>
